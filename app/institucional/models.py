b5990c857e0a18305ea98e414093b3009c12fe32
JulianDeco <juliandecoppet@gmail.com>
2025-08-20 09:39:41 -0300

    Se agregaron cambios en modelo usuario para crear superadmin

--- app/institucional/models.py
+++ app/institucional/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from django.contrib.auth.models import BaseUserManager
+
+# class UsuarioManager(BaseUserManager):
+#     def create_user(self, email, password=None, **extra_fields):
+#         if not email:
+#             raise ValueError('El email debe estar configurado.')
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, email, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
+#         extra_fields.setdefault('is_active', True)
+
+#         if extra_fields.get('is_staff') is not True:
+#             raise ValueError('Superuser must have is_staff=True.')
+#         if extra_fields.get('is_superuser') is not True:
+#             raise ValueError('Superuser must have is_superuser=True.')
+        
+#         return self.create_user(email, password, **extra_fields)
+
 class Usuario(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
@@ -9,6 +33,7 @@ class Usuario(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    objects = UsuarioManager()
     class Meta:
         db_table = 'institucional_usuarios'
         verbose_name = 'Usuario'
