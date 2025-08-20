20b6b705b8af8b4c125e21962c896ccbe7b83369
JulianDeco <juliandecoppet@gmail.com>
2025-08-20 16:27:10 -0300

    Se agregan a las rutas disponibles las vistas de change password y reset password que provee django

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from main.views import LoginEmailView, home, logout_view, redirect_based_group
@@ -8,11 +9,14 @@ urlpatterns = [
     
     path('redirect', redirect_based_group, name='redirect_login'),
     path('logout', logout_view, name='logout'),
-    path('login/', LoginEmailView.as_view(), name='login'),
-    
+    path('login', LoginEmailView.as_view(), name='login'),
+
     path('', home, name='home'),
     
     path('institucional/', include('institucional.urls')),
     path('academico/', include('academico.urls')),
     path('administracion/', include('administracion.urls')),
+
+    path("change-password", auth_views.PasswordChangeView.as_view()),
+    path('password-recovery', auth_views.PasswordResetView.as_view())
 ]
